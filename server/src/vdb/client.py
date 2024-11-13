2fa43defcbe4fb89b2b75b5d98fd98989bd26bed
mjaliz <mrph14@gmail.com>
2024-11-13 08:52:24 +0330

    pinecone cloud client added

--- server/src/vdb/client.py
+++ server/src/vdb/client.py
@@ -1,17 +1,20 @@
 import itertools
 import os
 from pinecone.grpc import PineconeGRPC, GRPCClientConfig
+from pinecone import Pinecone
 
 from server.src.vdb.models import ProductEmbed
 
 
 class VDBClient:
     def __init__(self):
-        self._pc = PineconeGRPC(api_key="pclocal")
-        self._host = os.environ["VDB_HOST"]
-        self._index = self._pc.Index(
-            host=self._host, grpc_config=GRPCClientConfig(secure=False)
-        )
+        # self._pc = PineconeGRPC(api_key=os.environ["VDB_API_KEY"])
+        # self._host = os.environ["VDB_HOST"]
+        # self._index = self._pc.Index(
+        #     host=self._host, grpc_config=GRPCClientConfig(secure=False)
+        # )
+        self._pc = Pinecone(api_key=os.environ["VDB_API_KEY"])
+        self._pc.Index("photojoo")
 
     @staticmethod
     def _chunks(iterable, batch_size=500):
